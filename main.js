2e8098b256515514716434b0667f98ba39bcc864
nasXion <acctaekwondoclub@gmail.com>
2021-06-02 13:57:03 +0600

    s

--- main.js
+++ main.js
@@ -20,6 +20,27 @@ init();
 
 
 
+var acc=document.getElementsByClassName('tkd');
+var i;
+var len = acc.length;
+for(i=0; i<len; i++){
+	acc[i].addEventListener('click', function(){
+		this.classList.toggle('active');
+		var answer= this.nextElementSibling;
+		if(answer.style.maxHeight){
+			answer.style.maxHeight=null;
+		}
+		else{
+			answer.style.maxHeight=answer.scrollHeight + 'px'
+		}
+	})
+}
+
+
+
+
+
+
 
 
 
